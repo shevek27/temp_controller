3072e5e972c3ce5ff3068e4551c1dece4973a6c3
juan ortega <jportega02@gmail.com>
2025-01-21 19:11:05 -0300

    implemented cpu frequency limiter based on temperature

--- temp_monitor.cpp
+++ temp_monitor.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <thread>
+#include <chrono>
 
 
 namespace fs = std::filesystem;
@@ -26,7 +28,7 @@ float read_cpu_temp(const std::string& filepath)
     return degrees;
 }
 
-std::vector<std::string> find_temperature_files()
+std::vector<std::string> find_hwmon_temperature_files()
 {
     std::vector<std::string> temp_files;
 
@@ -34,6 +36,7 @@ std::vector<std::string> find_temperature_files()
     {
         for (const auto& file : fs::directory_iterator(entry.path()))
         {
+            // npos = no position
             if (file.path().filename().string().find("temp") != std::string::npos &&
             file.path().filename().string().find("_input") != std::string::npos)
             {
@@ -45,10 +48,42 @@ std::vector<std::string> find_temperature_files()
     return temp_files;
 }
 
+std::string find_cpu_frequency_control_file()
+{
+    // specifically cpu0 but controls frequency for all cpu cores
+    std::string frequency_file = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq";
+    if (fs::exists(frequency_file))
+    {
+        return frequency_file;
+    }
+    else
+    {
+        std::cerr << "error: no cpu frequency control file found." << std::endl;
+        return "";
+    }
+
+}
+
+void set_cpu_frequency(const std::string& frequency_path, const std::string& frequency)
+{
+    std::ofstream frequency_file(frequency_path);
+    if (!frequency_file.is_open())
+    {
+        std::cerr << "ERROR: unable to open cpu frequency file at " << frequency_path << std::endl;
+        return;
+    }
+
+    else
+    {
+        frequency_file << frequency;
+        frequency_file.close();
+    }
+}
+
 
 int main()
 {
-    std::vector<std::string> temp_files = find_temperature_files();
+    std::vector<std::string> temp_files = find_hwmon_temperature_files();
 
     if (temp_files.empty())
     {
@@ -56,13 +91,43 @@ int main()
         return 1;
     }
 
-    for (const auto& file : temp_files)
+    std::string cpu_frequency_file = find_cpu_frequency_control_file();
+    if (cpu_frequency_file.empty())
     {
-        float temperature = read_cpu_temp(file);
-        if (temperature >= 0)
+        return 1;
+    }
+
+
+    while (true)
+    {
+        for (const auto& temp_file : temp_files)
         {
-            std::cout << "sensor: " << file << " | temperature: " << temperature << "°C" << std::endl;
+            float temperature = read_cpu_temp(temp_file);
+            if (temperature >= 0)
+            {
+                std::cout << "sensor: " << temp_file << " | temperature: " << temperature << "°C" << std::endl;
+
+                // adjust cpu frequency based on temperature
+                std::string max_frequency;
+                if (temperature < 50)
+                {
+                    max_frequency = "350000";                   
+                }
+                else if (temperature < 70)
+                {
+                    max_frequency = "180000";
+                }
+                else
+                {
+                    max_frequency = "120000";
+                }
+
+                set_cpu_frequency(cpu_frequency_file, max_frequency);
+                std::cout << "set cpu frequency to: " << max_frequency << std::endl;
+            }
         }
+        std::this_thread::sleep_for(std::chrono::seconds(3));
     }
     return 0;
+
 }
